7df34888bb3bcd99168162ec298120407da349c9
柯唯廉 <A11023044@live.yuntech.edu.tw>
2022-05-13 20:41:54 +0800

    fix:補一個Model

---
+++ Models/User_Message.cs
@@ -0,0 +1,13 @@
+﻿namespace WebCrawler.Models
+{
+    public class User_Message
+    {
+        public int UId { get; set; }
+        public string? UName { get; set; }
+        public string? UEmail { get; set; }
+        public int MesId { get; set; }
+        public string? Content { get; set; }
+        public DateTime? Date { get; set; }
+
+    }
+}
