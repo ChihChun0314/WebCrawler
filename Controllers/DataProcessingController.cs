56ebaeb0a19b9812e2ead1f56a890d449cb57650
A11023005 <A11023005@live.yuntech.edu.tw>
2022-05-25 14:18:48 +0800

    新增個人檢測紀錄

--- Controllers/DataProcessingController.cs
+++ Controllers/DataProcessingController.cs
@@ -4,21 +4,25 @@ using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Linq;
 using WebCrawler.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebCrawler.Controllers
 {
     public class DataProcessingController : Controller
     {
+        private readonly CrawlerContext DB;
+
+        public DataProcessingController(CrawlerContext _DB)
+        {
+            DB = _DB;
+            conn.ConnectionString = WebCrawler.Properties.Resources.ConnectionString;
+        }
         SqlCommand com = new SqlCommand();
         SqlDataReader dr;
         SqlConnection conn = new SqlConnection();
         List<User> userinfo = new List<User>();
         List<Crawler> crawlerinfo = new List<Crawler>();
         List<PostAnalysis> postinfo = new List<PostAnalysis>();
-        public DataProcessingController()
-        {
-            conn.ConnectionString = WebCrawler.Properties.Resources.ConnectionString;
-        }
 
         public IActionResult SetInterval()
         {
@@ -56,7 +60,7 @@ namespace WebCrawler.Controllers
             psi.FileName = @"C:\Users\李培聖\AppData\Local\Programs\Python\Python36\python.exe";
 
             // 2) Provide script and arguments
-            var script = @"C:\Users\李培聖\Desktop\Crawler\WebCrawler\Detect.py";
+            var script = @"Detect.py";
             var id = (int)HttpContext.Session.GetInt32("UserId");
             //var end = "10";
 
@@ -131,14 +135,14 @@ namespace WebCrawler.Controllers
                 int id = (int)HttpContext.Session.GetInt32("UserId");
                 conn.Open();
                 com.Connection = conn;
-                com.CommandText = $"SELECT TOP (1) [C_ID],[Content],[URL],[Web_name] FROM[Crawler].[dbo].[Crawler] WHERE [U_ID]={id} ORDER BY [Time] DESC";
+                com.CommandText = $"SELECT TOP (1) [Time],[Content],[URL],[Web_name] FROM[Crawler].[dbo].[Crawler] WHERE [U_ID]={id} ORDER BY [Time] DESC";
                 //  ORDER BY [Time] DESC
                 dr = com.ExecuteReader();
                 while (dr.Read())
                 {
                     crawlerinfo.Add(new Crawler()
                     {
-                        CId = (int)dr["C_ID"]
+                        Time = (DateTime?)dr["Time"]
                     ,
                         Content = dr["Content"].ToString()
                     ,
@@ -163,16 +167,16 @@ namespace WebCrawler.Controllers
                 int id = (int)HttpContext.Session.GetInt32("UserId");
                 conn.Open();
                 com.Connection = conn;
-                com.CommandText = $"SELECT TOP (1) A.C_ID, A.Content, C.URL, C.Web_name FROM[Crawler].[dbo].[Analysis] as A INNER JOIN[Crawler].[dbo].[Crawler] as C on A.C_ID = C.C_ID where[U_ID] ={id} ORDER BY [Time] DESC";
+                com.CommandText = $"SELECT TOP (1) C.Time, A.Content, C.URL, C.Web_name FROM[Crawler].[dbo].[Analysis] as A INNER JOIN[Crawler].[dbo].[Crawler] as C on A.C_ID = C.C_ID where[U_ID] ={id} ORDER BY [Time] DESC";
                 //  ORDER BY [Time] DESC
                 dr = com.ExecuteReader();
                 while (dr.Read())
                 {
                     postinfo.Add(new PostAnalysis()
                     {
-                        CId = ((int?)dr["C_ID"])
+                        Time = (DateTime?)dr["Time"]
                     ,
-                        Content = dr["Content"].ToString()
+                        PostContent = dr["Content"].ToString()
                     ,
                         Url = dr["URL"].ToString()
                     ,
@@ -186,6 +190,64 @@ namespace WebCrawler.Controllers
 
                 throw;
             }
+        }
+
+        public async Task<IActionResult> UserRecords()
+        {
+            List<PostAnalysis> PostAnalysis = new List<PostAnalysis>();
+            var message = from Crawler in DB.Crawlers
+                          join Analysis in DB.Analyses
+                          on Crawler.CId equals Analysis.CId
+                          select new { Crawler, Analysis };
+            var ms = await message.ToListAsync();
+            ms.Reverse();
+            foreach (var odj in ms)
+            {
+                PostAnalysis.Add(new PostAnalysis()
+                {
+                    AId = odj.Analysis.AId,
+                    CId = odj.Crawler.CId,
+                    TId = odj.Analysis.TId,
+                    PreContent = odj.Crawler.Content,
+                    PostContent = odj.Analysis.Content,
+                    Time = odj.Crawler.Time,
+                    Url = odj.Crawler.Url,
+                    WebName = odj.Crawler.WebName
+                });
+            }
+            return View(PostAnalysis);
+        }
+
+        public async Task<IActionResult> PreContent(int id)
+        {
+            var analysis = await DB.Analyses.Where(x => x.AId == id).FirstOrDefaultAsync();
+            var crawler = await DB.Crawlers.Where(x => x.CId == analysis.CId).FirstOrDefaultAsync();
+            PostAnalysis PostAnalysis = new PostAnalysis();
+            PostAnalysis.AId = analysis.AId;
+            PostAnalysis.CId = crawler.CId;
+            PostAnalysis.TId = analysis.TId;
+            PostAnalysis.PreContent = crawler.Content;
+            PostAnalysis.PostContent = analysis.Content;
+            PostAnalysis.Time = crawler.Time;
+            PostAnalysis.Url = crawler.Url;
+            PostAnalysis.WebName = crawler.WebName;
+            return View(PostAnalysis);
+        }
+
+        public async Task<IActionResult> PostContent(int id)
+        {
+            var analysis = await DB.Analyses.Where(x => x.AId == id).FirstOrDefaultAsync();
+            var crawler = await DB.Crawlers.Where(x => x.CId == analysis.CId).FirstOrDefaultAsync();
+            PostAnalysis PostAnalysis = new PostAnalysis();
+            PostAnalysis.AId = analysis.AId;
+            PostAnalysis.CId = crawler.CId;
+            PostAnalysis.TId = analysis.TId;
+            PostAnalysis.PreContent = crawler.Content;
+            PostAnalysis.PostContent = analysis.Content;
+            PostAnalysis.Time = crawler.Time;
+            PostAnalysis.Url = crawler.Url;
+            PostAnalysis.WebName = crawler.WebName;
+            return View(PostAnalysis);
         }
     }
 }
--- Models/PostAnalysis.cs
+++ Models/PostAnalysis.cs
@@ -8,7 +8,8 @@ namespace WebCrawler.Models
         public int AId { get; set; }
         public int? CId { get; set; }
         public int? TId { get; set; }
-        public string? Content { get; set; }
+        public string? PreContent { get; set; }
+        public string? PostContent { get; set; }
         public DateTime? Time { get; set; }
         public string? Url { get; set; }
         public string? WebName { get; set; }
