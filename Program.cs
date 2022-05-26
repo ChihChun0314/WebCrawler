e1ae7fd67808e87807ec48447fbb4bccac498777
A11023046 <a11023046@gemail.yuntech.edu.tw>
2022-05-27 04:52:18 +0800

    fix: 更新初始介面

--- Program.cs
+++ Program.cs
@@ -40,6 +40,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Backend}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=about}/{id?}");
 
 app.Run();
